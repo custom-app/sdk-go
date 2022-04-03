f9a23bd9d3bf6ae352fa318312fc0cbf5ad91d00
Oleg Shatnyuk <o.shatnuk@gmail.com>
2022-04-03 23:10:13 +0300

    drop docker dependency

--- go.mod
+++ go.mod
@@ -3,18 +3,14 @@ module github.com/custom-app/sdk-go
 go 1.15
 
 require (
-	github.com/Microsoft/go-winio v0.5.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/docker/docker v20.10.9+incompatible
-	github.com/docker/go-connections v0.4.0 // indirect
-	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/websocket v1.4.2
 	github.com/jackc/pgconn v1.6.4
 	github.com/jackc/pgx/v4 v4.8.1
 	github.com/minio/minio-go/v7 v7.0.12
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
-	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
 	google.golang.org/protobuf v1.25.0
 )
--- testutil/container/container.go
+++ testutil/container/container.go
@@ -4,171 +4,44 @@
 package container
 
 import (
-	"bytes"
-	"context"
 	"database/sql"
 	"fmt"
 	"github.com/custom-app/sdk-go/logger"
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/stdcopy"
 	_ "github.com/jackc/pgx/v4/stdlib"
-	"io/ioutil"
 	"time"
 )
 
-func findDatabase(cli *client.Client, containerName string) (*types.Container, error) {
-	list, err := cli.ContainerList(context.Background(), types.ContainerListOptions{All: true})
-	if err != nil {
-		return nil, err
-	}
-	for _, c := range list {
-		for _, n := range c.Names {
-			if n == containerName {
-				return &c, nil
-			}
-		}
-	}
-	return nil, fmt.Errorf("not found")
-}
-
-type execResult struct {
-	StdOut   string
-	StdErr   string
-	ExitCode int
-}
-
-func exec(cli *client.Client, containerID string, command []string) (types.IDResponse, error) {
-	config := types.ExecConfig{
-		AttachStderr: true,
-		AttachStdout: true,
-		Cmd:          command,
-	}
-
-	return cli.ContainerExecCreate(context.Background(), containerID, config)
-}
-
-func inspectExecResp(cli *client.Client, id string) (execResult, error) {
-	var execResult execResult
-
-	resp, err := cli.ContainerExecAttach(context.Background(), id, types.ExecStartCheck{})
-	if err != nil {
-		return execResult, err
-	}
-	defer resp.Close()
-
-	var outBuf, errBuf bytes.Buffer
-	outputDone := make(chan error)
-
-	go func() {
-		_, err = stdcopy.StdCopy(&outBuf, &errBuf, resp.Reader)
-		outputDone <- err
-	}()
-
-	select {
-	case err := <-outputDone:
-		if err != nil {
-			return execResult, err
-		}
-		break
-	}
-
-	stdout, err := ioutil.ReadAll(&outBuf)
-	if err != nil {
-		return execResult, err
-	}
-	stderr, err := ioutil.ReadAll(&errBuf)
-	if err != nil {
-		return execResult, err
-	}
-
-	res, err := cli.ContainerExecInspect(context.Background(), id)
-	if err != nil {
-		return execResult, err
-	}
-
-	execResult.ExitCode = res.ExitCode
-	execResult.StdOut = string(stdout)
-	execResult.StdErr = string(stderr)
-	return execResult, nil
-}
+var (
+	rootDb *sql.DB
+)
 
 // StartDatabase - создание базы данных в контейнере с именем containerName. указывается пользователь, пароль,
 // префикс имени базы данных, к которому будет добавлен unix-timestamp в миллисекундах и порт
-func StartDatabase(containerName, user, pass, databasePrefix string, port int) (*sql.DB, string, error) {
+func StartDatabase(rootUser, rootPassword, rootDbName, user, pass, databasePrefix string, port int) (*sql.DB, string, error) {
 	now := time.Now().UnixNano() / 1e+6
-	c, err := client.NewEnvClient()
+	var err error
+	rootDb, err = sql.Open("pgx", fmt.Sprintf("host=localhost port=%d user=%s password=%s dbname=%s sslmode=disable",
+		port, rootUser, rootPassword, rootDbName))
 	if err != nil {
 		return nil, "", err
 	}
-	cont, err := findDatabase(c, containerName)
-	if err != nil {
-		return nil, "", err
-	}
-
 	databaseName := fmt.Sprintf("%s_%d", databasePrefix, now)
-	logger.Log("creating database", databaseName)
-	id, err := exec(c, cont.ID, []string{
-		"psql",
-		"-U",
-		"postgres",
-		"-c",
-		fmt.Sprintf("create database %s;", databaseName),
-	})
-	if err != nil {
+	if _, err := rootDb.Exec(fmt.Sprintf("create database %s", databaseName)); err != nil {
 		return nil, "", err
 	}
-	res, err := inspectExecResp(c, id.ID)
-	if err != nil {
+	if _, err := rootDb.Exec(fmt.Sprintf("do $$ BEGIN CREATE USER %s SUPERUSER PASSWORD '%s';"+
+		"  EXCEPTION WHEN DUPLICATE_OBJECT THEN RAISE NOTICE 'not creating user'; END $$", user, pass)); err != nil {
 		return nil, "", err
 	}
-	logger.Log("first exec stdout: ", res.StdOut)
-	logger.Log("first exec stderr: ", res.StdErr)
-	if res.ExitCode != 0 {
-		return nil, "", fmt.Errorf("first exec failed: %d", res.ExitCode)
-	}
-	id, err = exec(c, cont.ID, []string{
-		"psql",
-		"-U",
-		"postgres",
-		"-c",
-		fmt.Sprintf("DO $$\nBEGIN\nCREATE USER %s SUPERUSER PASSWORD '%s';\n"+
-			"EXCEPTION WHEN DUPLICATE_OBJECT THEN\nRAISE NOTICE 'not creating user';\nEND\n$$;\n"+
-			"GRANT ALL PRIVILEGES ON DATABASE %s TO %s;\n", user, pass, databaseName, user),
-	})
-	if err != nil {
-		if err := StopDatabase(databaseName, containerName); err != nil {
-			logger.Log("stop after failed start err12: ", err)
-		}
+	if _, err := rootDb.Exec(fmt.Sprintf("GRANT ALL PRIVILEGES ON DATABASE %s TO %s", databaseName, user)); err != nil {
 		return nil, "", err
-	}
-	res, err = inspectExecResp(c, id.ID)
-	if err != nil {
-		if err := StopDatabase(databaseName, containerName); err != nil {
-			logger.Log("stop after failed start err: ", err)
-		}
-		return nil, "", err
-	}
-	logger.Log("second exec stdout: ", res.StdOut)
-	logger.Log("second exec stderr: ", res.StdErr)
-	if res.ExitCode != 0 {
-		if err := StopDatabase(databaseName, containerName); err != nil {
-			logger.Log("stop after failed start err3: ", err)
-		}
-		return nil, "", fmt.Errorf("second exec failed: %d", res.ExitCode)
 	}
 
-	if err := c.Close(); err != nil {
-		if err := StopDatabase(databaseName, containerName); err != nil {
-			logger.Log("stop after failed start err4: ", err)
-		}
-		return nil, "", err
-	}
 	connectionString := fmt.Sprintf("host=localhost port=%d user=%s password=%s dbname=%s sslmode=disable",
 		port, user, pass, databaseName)
 	db, err := sql.Open("pgx", connectionString)
 	if err != nil {
-		if err := StopDatabase(databaseName, containerName); err != nil {
+		if err := StopDatabase(databaseName); err != nil {
 			logger.Log("stop after failed start err5: ", err)
 		}
 		return nil, "", err
@@ -186,7 +59,7 @@ func StartDatabase(containerName, user, pass, databasePrefix string, port int) (
 	}()
 	select {
 	case <-timeout:
-		if err := StopDatabase(databaseName, containerName); err != nil {
+		if err := StopDatabase(databaseName); err != nil {
 			logger.Log("stop after failed start err6: ", err)
 		}
 		return nil, "", fmt.Errorf("ping timeout")
@@ -197,33 +70,9 @@ func StartDatabase(containerName, user, pass, databasePrefix string, port int) (
 }
 
 // StopDatabase - удаление базы данных
-func StopDatabase(containerName, d string) error {
-	c, err := client.NewEnvClient()
-	if err != nil {
+func StopDatabase(dbName string) error {
+	if _, err := rootDb.Exec(fmt.Sprintf("drop database %s", dbName)); err != nil {
 		return err
 	}
-	cont, err := findDatabase(c, containerName)
-	if err != nil {
-		return err
-	}
-	id, err := exec(c, cont.ID, []string{
-		"psql",
-		"-U",
-		"postgres",
-		"-c",
-		fmt.Sprintf("drop database %s;\n", d),
-	})
-	if err != nil {
-		return err
-	}
-	res, err := inspectExecResp(c, id.ID)
-	if err != nil {
-		return err
-	}
-	logger.Log("drop exec stdout: ", res.StdOut)
-	logger.Log("drop exec stderr: ", res.StdErr)
-	if res.ExitCode != 0 {
-		return fmt.Errorf("drop exec failed: %d", res.ExitCode)
-	}
-	return c.Close()
+	return nil
 }
